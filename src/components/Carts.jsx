57527b009de8e767d8993c7606270795b6197a3e
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-03-24 00:51:02 +0530

    working on cart features

--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -12,17 +12,23 @@ const Carts = () => {
 
     let [showexit, setshowexit] = useState(true)
 
-    return (<div>
+    return (<div className="">
         <div className=" mt-48 mx-56 max-sm:mx-10 max-md:mx-12 max-lg:mx-28 max-xl:mx-52 max-2xl:mx-64 items-center ">
-            <div className=" px-8">
+            <div className="">
                 {selector.length > 0 ?
-                    <div>
+                    <div className="">
                         <h1 className="text-center text-6xl font-semibold my-10">Cart</h1>
-                        < Restaurantgroupcard data={selector} exit={showexit} />
-                    </div>
-                    : <EmptyCart />
-                }</div>
 
+                        < Restaurantgroupcard data={selector} exit={showexit} />
+
+                        <div className=" ">
+                            <h1 className="w-[32em] py-2 px-8 font-semibold text-2xl
+                 bg-[#f3c77b] text-gray-900 fixed  bottom-0">Total - ₹ 1,500</h1>
+                        </div>
+
+                    </div>
+                    : <EmptyCart className="shadow-none " />
+                }</div>
         </div>
     </div >
     )
--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -6,11 +6,12 @@ export const EmptyCart = () => {
     return (
         <div>
             <div className='flex justify-center'>
-                <img className='w-[35em] h-80' src={preview} alt="imnage" />
+                <img className='w-[28em] ' src={preview} alt="imnage" />
             </div>
             <div className='text-center'>
                 <h1 className='py-4 text-4xl font-semibold font-mono'>Your Cart is empty</h1>
-                <Link to={'/home'}><button className='uppercase font-medium rounded-md text-lg text-center px-4 p-2 border-2 border-[#fca729] text-[#fca729] hover:bg-[#fca729] hover:text-white'>Browse Restaurent</button></Link>
+                <Link to={'/home'}><button className='uppercase font-medium rounded-md text-lg text-center px-4 p-2 border-2 border-[#fca729] text-[#fca729]
+                 hover:bg-[#fca729] hover:text-white'>Browse Restaurent</button></Link>
             </div>
         </div>
     )
